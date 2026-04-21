1bf75046a38e6ff79d9bf4e74b51efd8c6230fd6
Master Spike <svf18062006@gmail.com>
2026-04-20 22:08:43 -0300

    tests

--- pages/project-ghost/js/data.js
+++ pages/project-ghost/js/data.js
@@ -266,8 +266,8 @@ const chaptersData = {
                             mobile: 1
                         },
                         pageEnd: {
-                            desktop: 5,
-                            mobile: 5
+                            desktop: 30,
+                            mobile: 30
                         },
                         googleDocsUrl: "https://docs.google.com/document/d/1J3BpHaeWOi7-Dz8iaLW-i4VxXWLOK3kwb8EN9WZpRaQ/edit?usp=sharing",
                         pdf: {
@@ -276,18 +276,63 @@ const chaptersData = {
                         },
                         scenes: [
                             {
-                                id: "clock_dream",
-                                label: "Sonho do relógio",
+                                id: "capa",
+                                label: "Cap",
                                 description: "Vazio, escuridão, fumaça e sensação de tempo suspenso.",
                                 from: 1,
-                                to: 5,
-                                background: "assets/images/website/background_main3.png",
-                                audio: "assets/audio/clock_ambience.mp3",
+                                to: 2,
+                                background: "none",
+                                audio: "none",
                                 theme: {
                                     accent: "#ff3a3a",
                                     panel: "rgba(34, 8, 10, 0.78)",
                                     text: "#fff5f5",
                                     muted: "#ffd0d0"
+                                }
+                            },
+                            {
+                                id: "clock_dream",
+                                label: "Sonho do relógio",
+                                description: "Vazio, escuridão, fumaça e sensação de tempo suspenso.",
+                                from: 3,
+                                to: 5,
+                                background: "assets/cenas/vazio.png",
+                                audio: "assets/audio/ordem-paranormal/acaso.mp3",
+                                theme: {
+                                    accent: "#000000",
+                                    panel: "rgba(44, 44, 44, 0.78)",
+                                    text: "#ffffff",
+                                    muted: "#454141"
+                                }
+                            },
+                            {
+                                id: "Quarto_do_haruki",
+                                label: "Quarto do Haruki",
+                                description: "Mofo, escuro e o som do despertador",
+                                from: 6,
+                                to: 6,
+                                background: "assets/cenas/quarto_haruki.png",
+                                audio: "assets/audio/ordem-paranormal/curiosidade.mp3",
+                                theme: {
+                                    accent: "#000812",
+                                    panel: "rgba(0, 24, 64, 0.78)",
+                                    text: "#ffffff",
+                                    muted: "#001c22"
+                                }
+                            },
+                            {
+                                id: "Quarto_do_haruki",
+                                label: "Quarto do Haruki",
+                                description: "Mofo, escuro e o som do despertador",
+                                from: 7,
+                                to: 7,
+                                background: "assets/cenas/quarto_haruki.png",
+                                audio: "assets/audio/ordem-paranormal/curiosidade.mp3",
+                                theme: {
+                                    accent: "#000812",
+                                    panel: "rgba(0, 24, 64, 0.78)",
+                                    text: "#ffffff",
+                                    muted: "#001c22"
                                 }
                             }
                         ]
