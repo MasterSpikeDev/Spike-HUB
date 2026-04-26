8e36ee7bbc8da6fe19fda3087c57429863296685
Master Spike <svf18062006@gmail.com>
2026-04-26 17:13:40 -0300

    update portfolio

--- posts.js
+++ posts.js
@@ -2,6 +2,92 @@
 // Expondo globalmente (sem ES Modules)
 window.POSTS = [
     {
+        title: { pt: "Item Physics & More Add-On", en: "Item Physics & More Add-On" },
+        desc: {
+            pt: "Adicione movimento ao seu mundo com o Physics Add-On: itens realistas, árvores caindo, mortes limpas de criaturas e detritos — tudo ajustável nas configurações.",
+            en: "Add motion to your world with Physics Add-On: realistic items, falling trees, clean mob deaths, and debris-tunable in settings"
+        },
+        img: "assets/portfolio/post8.png",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/item-physics-more-add-on" },
+            { label: { pt: "Youtube", en: "Youtube" }, href: "https://www.youtube.com/watch?v=rPoOsSwoR0U" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/blocklab-studios/item-physics--more-add--on/448e727e-6440-4518-9967-09db195c7209" }
+
+        ],
+        videos: []
+    },
+    {
+        title: { pt: "Vibrant Adventures", en: "Vibrant Adventures" },
+        desc: {
+            pt: "Adentre um mundo radiante criado para celebrar os visuais vibrantes de Minecraft. De paisagens exuberantes a cavernas brilhantes, cada canto transborda luz, cor e descobertas.",
+            en: "Step into a radiant world crafted to celebrate Minecraft’s Vibrant Visuals. From lush landscapes to glowing caves, every corner bursts with light, color, and discovery."
+        },
+        img: "assets/portfolio/post4.png",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/vibrant-adventures" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/minecraft/vibrant-adventures/f85c0b48-e3aa-4156-af13-dec7537e79ae" }
+        ],
+        videos: []
+    },
+    {
+        title: { pt: "BiomeCraft", en: "BiomeCraft" },
+        desc: {
+            pt: "Expanda seu mundo com os quatro biomas deslumbrantes do BiomeCraft e dois clássicos repaginados. Sequóias imponentes, florestas tropicais envoltas em névoa, pradarias douradas e veldts ressequidos pelo sol transbordam com blocos exclusivos e cenários inéditos.",
+            en: "Expand your world with BiomeCraft’s four breathtaking biomes and two re-imagined classics. Towering redwoods, mist-laden rainforests, golden prairies, and sun-parched veldts each overflow with bespoke blocks and fresh scenery."
+        },
+        img: "assets/portfolio/post2.jpg",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/biomecraft" },
+            { label: { pt: "Trailer (YouTube)", en: "Trailer (YouTube)" }, href: "https://www.youtube.com/watch?v=vJ_1bDuXBbo" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/blocklab-studios/biomecraft-add--on-1.0/5753fc95-7022-400c-a0d4-576d07ecc2aa" }
+
+        ],
+        videos: []
+    },
+    {
+        title: { pt: "Better Structures", en: "Better Structures" },
+        desc: {
+            pt: "Hora da exploração!",
+            en: "Exploration time!"
+        },
+        img: "assets/portfolio/post3.jpg",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/better-structures" },
+            { label: { pt: "Trailer (YouTube)", en: "Trailer (YouTube)" }, href: "https://www.youtube.com/watch?v=YBXKErlHxKs" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/blocklab-studios/better-structures-add--on/5e6039aa-a4a1-4508-880c-564129ce04bc" }
+
+        ],
+        videos: []
+    },
+    {
+        title: { pt: "More Enchants", en: "More Enchants" },
+        desc: {
+            pt: "Canalize o poder bruto com mais de 26 novos encantamentos. Forje-os no misterioso ALTAR DA ESCULTURA e veja equipamentos comuns ascenderem ao status lendário.",
+            en: "Channel raw power with 26+ brand-new enchantments. Forge them at the mysterious SCULK ALTAR and watch ordinary gear ascend to legendary status."
+        },
+        img: "assets/portfolio/post5.png",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/more-enchants" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/blocklab-studios/more-enchants-add--on/dfa41d12-3d83-4e4c-b3f0-dd5e91e85340" }
+
+        ],
+        videos: []
+    },
+    {
+        title: { pt: "More Utilities", en: "More Utilities" },
+        desc: {
+            pt: "Melhore sua experiência de sobrevivência.",
+            en: "Upgrade your Survival Experience."
+        },
+        img: "assets/portfolio/post6.png",
+        links: [
+            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/more-utilities" },
+            { label: { pt: "Minecraft Marketplace", en: "Minecraft Marketplace" }, href: "https://www.minecraft.net/en-us/marketplace/pdp/blocklab-studios/more-utilities-2.0-add--on/7a7dc34b-5633-4527-88fa-3661eaa6771e" }
+
+        ],
+        videos: []
+    },
+        {
         title: { pt: "Sheikah Legacy - Mapa de Zelda", en: "Sheikah Legacy - Zelda Map" },
         desc: {
             pt: "Bem-vindo ao Sheikah Legacy! Mergulhe em uma aventura épica no mundo de Hyrule",
@@ -28,70 +114,4 @@ window.POSTS = [
         ],
         videos: []
     },
-    {
-        title: { pt: "Vibrant Adventures", en: "Vibrant Adventures" },
-        desc: {
-            pt: "Explore um mundo repleto de cores e vida.",
-            en: "Explore a World Bursting with Color and Life"
-        },
-        img: "assets/portfolio/post4.png",
-        links: [
-            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/vibrant-adventures" }
-
-        ],
-        videos: []
-    },
-    {
-        title: { pt: "BiomeCraft", en: "BiomeCraft" },
-        desc: {
-            pt: "Expanda seu mundo com os quatro biomas de tirar o fôlego do BiomeCraft e dois clássicos reinventados.",
-            en: "Expand your world with BiomeCraft’s four breathtaking biomes and two re-imagined classics. "
-        },
-        img: "assets/portfolio/post2.jpg",
-        links: [
-            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/biomecraft" },
-            { label: { pt: "Trailer (YouTube)", en: "Trailer (YouTube)" }, href: "https://www.youtube.com/watch?v=vJ_1bDuXBbo" }
-
-        ],
-        videos: []
-    },
-    {
-        title: { pt: "Better Structures", en: "Better Structures" },
-        desc: {
-            pt: "Hora da exploração!",
-            en: "Exploration time!"
-        },
-        img: "assets/portfolio/post3.jpg",
-        links: [
-            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/better-structures" }
-
-        ],
-        videos: []
-    },
-    {
-        title: { pt: "More Enchants", en: "More Enchants" },
-        desc: {
-            pt: "Encantamentos, encantamentos, encantamentos!",
-            en: "Enchantments, enchantments, enchantmens!"
-        },
-        img: "assets/portfolio/post5.png",
-        links: [
-            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/more-enchants" }
-
-        ],
-        videos: []
-    },
-    {
-        title: { pt: "More Utilities", en: "More Utilities" },
-        desc: {
-            pt: "Melhore sua experiência de sobrevivência.",
-            en: "Upgrade your Survival Experience."
-        },
-        img: "assets/portfolio/post6.png",
-        links: [
-            { label: { pt: "Eternal Creations", en: "Eternal Creations" }, href: "https://eternalcreations.games/games/more-utilities" }
-
-        ],
-        videos: []
-    }
 ];
