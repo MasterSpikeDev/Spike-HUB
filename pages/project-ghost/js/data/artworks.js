46578a1a27be5c0bccc362e1b709f0728e3fb556
Master Spike <svf18062006@gmail.com>
2026-06-14 20:43:12 -0300

    novas artes cadastradas

--- pages/project-ghost/js/data/artworks.js
+++ pages/project-ghost/js/data/artworks.js
@@ -13,7 +13,7 @@ export const artworks = [
         id: 10,
         title: "Haruki Takamatsu - A vingança",
         artType: "arte oficial",
-        image: "assets/images/arts/artes/10.png",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/vinganca.png",
         description: "''Agora eu vou te ensinar a diferença entre fogo e vingança. Corre, eu gosto quando o medo acende primeiro que minhas chamas''",
         year: "2026",
         artist: "Official Art - Visual Direction / Samuel Victor",
@@ -23,7 +23,7 @@ export const artworks = [
         id: 13,
         title: "Fumiko Emi - A promessa",
         artType: "arte oficial",
-        image: "assets/images/arts/artes/11.png",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/negacao.png",
         description: "''Promessas feitas na nossa organização não são palavras jogadas ao vento. São escolhas que continuam valendo mesmo quando você se arrepende.''",
         year: "2026",
         artist: "Official Art - Visual Direction / Samuel Victor",
@@ -31,30 +31,50 @@ export const artworks = [
     },
     {
         id: 1,
-        title: "Haruki Takamatsu - Traje Anti-Chama (Parte 1)",
+        title: "Haruki Takamatsu - Traje Anti-Chama",
         artType: "concept art",
-        image: "assets/images/arts/concept-arts/haruki/1.png",
-        description: "Arte conceitual gerada por IA, do traje anti-chama de Haruki Takamatsu / Ember Blade, parte 1",
+        image: "assets/images/arts/artes-publicadas/concept-arts/haruki/haruki-traje-batalha-concept-art.png",
+        description: "Arte conceitual, do traje anti-chama de Haruki Takamatsu",
         year: "2025",
         artist: "Concept Art - IA",
         tags: ["haruki"]
     },
     {
-        id: 2,
-        title: "Haruki Takamatsu - Traje Anti-Chama (Parte 2)",
+        id: 1,
+        title: "Haruki Takamatsu - Traje Escolar",
         artType: "concept art",
-        image: "assets/images/arts/concept-arts/haruki/2.png",
-        description: "Arte conceitual gerada por IA, do traje anti-chama de Haruki Takamatsu, agora com sua iconica touca e mascará / Ember Blade, parte 1",
+        image: "assets/images/arts/artes-publicadas/concept-arts/haruki/haruki-vida-escolar-concept-art.png",
+        description: "Arte conceitual, de Haruki Takamatsu traje escolar",
+        year: "2025",
+        artist: "Concept Art - IA",
+        tags: ["haruki"]
+    },
+    {
+        id: 1,
+        title: "Haruki Takamatsu - Traje Divisão Fantasma",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/haruki/haruki-divisao fantasma-concept-art.png",
+        description: "Arte conceitual, de Haruki Takamatsu traje divisão fantasma",
         year: "2025",
         artist: "Concept Art - IA",
         tags: ["haruki"]
     },
     {
         id: 3,
-        title: "Fumiko Emi - Concept Art 1",
+        title: "Fumiko Emi Concept Art - Divisão Fantasma",
         artType: "concept art",
-        image: "assets/images/arts/concept-arts/fumiko/1.png",
-        description: "Arte conceitual gerada por IA, de Fumiko Emi, roupas escolares",
+        image: "assets/images/arts/artes-publicadas/concept-arts/fumiko/fumiko-divisao-fantasma-concept-art.png",
+        description: "Arte conceitual, de Fumiko Emi, Divisão Fantasma",
+        year: "2025",
+        artist: "Concept Art - IA",
+        tags: ["fumiko"]
+    },
+    {
+        id: 3,
+        title: "Fumiko Emi Concept Art - Traje Escolar",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/fumiko/fumiko-vida-escolar-concept-art.png",
+        description: "Arte conceitual, de Fumiko Emi, Traje Escolar",
         year: "2025",
         artist: "Concept Art - IA",
         tags: ["fumiko"]
@@ -63,27 +83,17 @@ export const artworks = [
         id: 9,
         title: "Genki Kato (Art Oficial)",
         artType: "arte oficial",
-        image: "assets/images/arts/artes/9.png",
+        image: "assets/images/arts/artes-publicadas/classico/capitulo-3-genki-fan-art.png",
         description: "Arte feita em sua primeira versão da história (The Ghoul`s Prophecy), na arte temos Genki Kato, um dos membros da Divisão Fantasma. Curiosidade: Essa arte foi feita para apresentar o lançamento do capitulo 3.",
         year: "2022",
         artist: "Arte Oficial - Moon Honey",
         tags: ["genki"]
     },
     {
-        id: 4,
-        title: "Fumiko Emi - Concept Art 2",
-        artType: "concept art",
-        image: "assets/images/arts/concept-arts/fumiko/2.png",
-        description: "Arte conceitual gerada por IA, de Fumiko Emi",
-        year: "2025",
-        artist: "Concept Art - IA",
-        tags: ["fumiko"]
-    },
-    {
         id: 5,
         title: "Haruki Takamatsu (Clássico)",
         artType: "arte oficial",
-        image: "assets/images/arts/artes/2.png",
+        image: "assets/images/arts/artes-publicadas/classico/haruki-classico.png",
         description: "Arte feita pelo Autor em sua primeira versão da história (The Ghoul`s Prophecy), na arte temos Haruki Clássico e Nero Clássico. Curiosidade: Essa foi a primeira arte e o primeiro conceito de criar uma obra própria.",
         year: "2021",
         artist: "Arte Oficial",
@@ -93,7 +103,7 @@ export const artworks = [
         id: 6,
         title: "Fumiko Emi (Clássica)",
         artType: "arte oficial",
-        image: "assets/images/arts/artes/3.png",
+        image: "assets/images/arts/artes-publicadas/classico/fumiko-classica.png",
         description: "Arte feita em sua primeira versão da história (The Ghoul`s Prophecy), na arte temos Fumiko Emi belíssima, usando sua primeira versão de seu poder. Curiosidade: Na primeira versão, Fumiko Emi teria poderes de dar vida a brinquedos danificados, e usa-los para criar seu proprio mini-exercito para batalhas. Descontinuado por amadurecimento da obra e questões de balanceamento (era um poder fraco demais).",
         year: "2022",
         artist: "Arte Oficial - Moon Honey",
@@ -103,7 +113,7 @@ export const artworks = [
         id: 7,
         title: "Fumiko Emi (Fan-Art)",
         artType: "fan art",
-        image: "assets/images/arts/artes/4.png",
+        image: "assets/images/arts/artes-publicadas/fan-art/fumiko-shurros.png",
         description: "Arte feita em sua primeira versão da história (The Ghoul`s Prophecy), na arte temos Fumiko Emi belíssima. Curiosidade: O antigo design de Fumiko era com longos cabelos vermelho carmesin completamente solto e despojado, e o uniforme escolar era azul vibrante. ''ruivinha muito lindinha espero que tenha acertado algo do design delakkkkk''.",
         year: "2022",
         artist: "Fan Art - Shurros",
@@ -113,11 +123,164 @@ export const artworks = [
         id: 8,
         title: "Fumiko roubando batatinhas (Fan-Art)",
         artType: "fan art",
-        image: "assets/images/arts/artes/8.png",
+        image: "assets/images/arts/artes-publicadas/fan-art/haruki-fumiko-batatinhas-fan-art.png",
         description: "Arte feita em sua primeira versão da história (The Ghoul`s Prophecy), na arte temos Fumiko Emi roubando as iconicas batatinhas fritas de Nero.",
         year: "2022",
         artist: "Fan Art - Ludivine / Yellow Mariana",
         tags: ["fumiko", "nero", "haruki"]
+    },
+    {
+        title: "Naomi Suzuki - Colapso",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/colapso.png",
+        description: "Arte de Naomi Suzuki, utilizando seus poderes.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["naomi"]
+    },
+    {
+        title: "A Divisão Fantasma",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/divisao_fantasma.png",
+        description: "Banner com todos os 5 membros da Divisão Fantasma, em ordens de cor: Genki Kato, Ichiko Kurugane, Haruki Takamatsu, Fumiko Emi e Naomi Suzuki. ''Os olhos, são a janela da alma, mas os meus...''",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["genki", "ichiko", "haruki", "fumiko", "naomi"]
+    },
+    {
+        title: "Haruki e Fumiko - Primeira Faísca",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/fumiko_e_haruki_brilho_nos_olhos.png",
+        description: "Algumas histórias começam com uma grande explosão. Outras começam com uma simples faísca. ✨",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["haruki", "fumiko"]
+    },
+    {
+        title: "Haruki e os Honey Crystal Chips",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/haruki_comendo_honey_crystal_chips.png",
+        description: "Haruki luta pelo último chip do pacote",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["haruki"]
+    },
+    {
+        title: "Cemitério Carmesim",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/haruki-takamatsu-1.png",
+        description: "Algumas sepulturas guardam os mortos. Outras guardam monstros que ainda não decidiram despertar.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["haruki"]
+    },
+    {
+        title: "Asas do Vazio",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/naomi_borboletas.png",
+        description: "Quanto mais ela tentava manter tudo unido, mais sentia seu próprio mundo se desfazer.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["naomi"]
+    },
+    {
+        title: "O Pilar Inabalável",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/pilar.png",
+        description: "A verdadeira força não está em nunca cair. Está em continuar carregando os outros mesmo depois de cair.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["genki"]
+    },
+    {
+        title: "Recrutamento",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/recrutamento.png",
+        description: "Liderança não é estar à frente. É continuar caminhando mesmo quando ninguém sabe qual é o caminho.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["fumiko"]
+    },
+    {
+        title: "Sorriso de Sangue",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/arte-oficial/sangue.png",
+        description: "Se o mundo quer um monstro, então vou escolher que tipo de monstro eu serei.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["ichiko"]
+    },
+    {
+        title: "Prólogo",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/capas/project_ghost/sem_logo/prologo.png",
+        description: "Toda história possui um começo. Esta começou quando o tempo parou.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["relogio"]
+    },
+    {
+        title: "Capítulo 1 — Faísca",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/capas/project_ghost/sem_logo/capitulo_1_faisca.png",
+        description: "Até o maior incêndio começou como uma simples faísca.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["haruki"]
+    },
+    {
+        title: "Capítulo 2 — Marionete",
+        artType: "arte oficial",
+        image: "assets/images/arts/artes-publicadas/capas/project_ghost/sem_logo/capitulo_2_marionete.png",
+        description: "O mais assustador não é descobrir que existe um manipulador. É descobrir que você sempre esteve preso aos fios dele.",
+        year: "2026",
+        artist: "Official Art - Visual Direction / Samuel Victor",
+        tags: ["nero"]
+    },
+    {
+        title: "Genki Kato - Divisão Fantasma",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/genki/genki-kato-concept-art.png",
+        description: "Arte conceitual, do traje Divisão Fantasma Genki Kato",
+        year: "2026",
+        artist: "Concept Art - IA",
+        tags: ["genki"]
+    },
+    {
+        title: "Ichiko Kurogane - Divisão Fantasma",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/ichiko/ichiko-kurogane-concept-art.png",
+        description: "Arte conceitual, do traje Divisão Fantasma Ichiko Kurogane",
+        year: "2026",
+        artist: "Concept Art - IA",
+        tags: ["ichiko"]
+    },
+    {
+        title: "Naomi Suzuki - Divisão Fantasma",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/naomi/naomi-suzuki-concept-art.png",
+        description: "Arte conceitual, do traje Divisão Fantasma Naomi Suzuki",
+        year: "2026",
+        artist: "Concept Art - IA",
+        tags: ["naomi"]
+    },
+    {
+        title: "Nero Claudius - Fisico",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/nero/nero_fisico.png",
+        description: "Arte conceitual, Nero em forma fisica",
+        year: "2026",
+        artist: "Concept Art - IA",
+        tags: ["nero"]
+    },
+    {
+        title: "Nero Claudius - Spirit",
+        artType: "concept art",
+        image: "assets/images/arts/artes-publicadas/concept-arts/nero/nero_spirit.png",
+        description: "Arte conceitual, Nero em forma de Spirit",
+        year: "2026",
+        artist: "Concept Art - IA",
+        tags: ["nero"]
     }
 ];
 // Dados dos capítulos (mantidos iguais)
